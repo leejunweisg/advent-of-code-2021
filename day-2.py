c57f580a3c3465ad97c23e25dc98d356c38f1db9
Lee Jun Wei <58636401+leejunweisg@users.noreply.github.com>
2021-12-05 11:56:48 +0800

    added day 2

---
+++ day-2.py
@@ -0,0 +1,47 @@
+def read_input():
+	lines = []
+	with open("input.txt") as fp:
+		lines = fp.read().splitlines()
+	return lines
+
+
+def p1(lines):
+	horizontal = 0
+	depth = 0
+
+	for line in lines:
+		direction, num = tuple(line.split(" "))
+		num = int(num)
+		if direction == 'forward':
+			horizontal += num
+		elif direction == 'down':
+			depth += num
+		else:
+			depth -= num
+
+	return horizontal * depth
+
+
+def p2(lines):
+	aim = 0
+	horizontal = 0
+	depth = 0
+
+	for line in lines:
+		direction, num = tuple(line.split(" "))
+		num = int(num)
+		if direction == 'down':
+			aim += num
+		elif direction == 'up':
+			aim -= num
+		else:
+			horizontal += num
+			depth += (aim * num)
+
+	return horizontal * depth
+
+
+if __name__ == "__main__":
+	lines = read_input()
+	print(p1(lines))
+	print(p2(lines))
